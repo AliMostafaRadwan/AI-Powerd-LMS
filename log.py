9c5431c5e898cc337cf138761a7af2eff4113864
AliMostafaRadwan <omar12ali12radwan@gmail.com>
2023-03-11 17:50:49 +0200

    final

--- log.py
+++ log.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
+from streamlit_elements import elements, mui, html
+from streamlit_elements import dashboard
 
 # Security
 #passlib,hashlib,bcrypt,scrypt
@@ -37,7 +39,7 @@ def view_all_users():
     return data
 
 def login():
-    
+    st.subheader("Login Section")
     username = st.text_input("User Name")
     password = st.text_input("Password",type='password')
     if st.button("Login"):
@@ -47,26 +49,27 @@ def login():
 
         result = login_user(username,check_hashes(password,hashed_pswd))
         if result:
-
+            # redirect to home page
             st.success("Logged In as {}".format(username))
-
-            task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
-            if task == "Add Post":
-                st.subheader("Add Your Post")
-                data = view_all_users()
-                data = pd.DataFrame(data,columns=["Username","Password"])
-                st.text(data)
-                
-
-            elif task == "Analytics":
-                st.subheader("Analytics")
-            elif task == "Profiles":
-                st.subheader("User Profiles")
-                user_result = view_all_users()
-                clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-                st.dataframe(clean_db)
+            st.subheader("User Profiles")
+            user_result = view_all_users()
+            clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
+            st.dataframe(clean_db)
+            
         else:
             st.warning("Incorrect Username/Password")
+        
+def signup():
+    st.subheader("Create New Account")
+    new_user = st.text_input("Username")
+    new_password = st.text_input("Password",type='password')
+
+    if st.button("Signup"):
+        create_usertable()
+        add_userdata(new_user,make_hashes(new_password))
+        st.success("You have successfully created a valid Account")
+        st.info("Go to Login Menu to login")
+
     
 
 def main():
@@ -74,7 +77,6 @@ def main():
 
     st.set_page_config(layout="wide")
 
-    st.header("Custom tab component for on-hover navigation bar")
     st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
 
@@ -84,85 +86,15 @@ def main():
 
     if tabs =='Dashboard':
         st.title("Navigation Bar")
-        st.write('Name of option is {}'.format(tabs))
+        
+        
 
     elif tabs == 'login':
         login()
 
 
     elif tabs == 'signup':
-        st.subheader("Create New Account")
-        new_user = st.text_input("Username")
-        new_password = st.text_input("Password",type='password')
-
-        if st.button("Signup"):
-            create_usertable()
-            add_userdata(new_user,make_hashes(new_password))
-            st.success("You have successfully created a valid Account")
-            st.info("Go to Login Menu to login")
-
-         
-
-# def main():
-# 	"""Simple Login App"""
-
-# 	st.title("Simple Login App")
-
-# 	menu = ["Home","Login","SignUp"]
-# 	choice = st.sidebar.selectbox("Menu",menu)
-
-# 	if choice == "Home":
-# 		st.subheader("Home")
-
-# 	elif choice == "Login":
-# 		st.subheader("Login Section")
-
-# 		username = st.sidebar.text_input("User Name")
-# 		password = st.sidebar.text_input("Password",type='password')
-# 		if st.sidebar.checkbox("Login"):
-# 			# if password == '12345':
-# 			create_usertable()
-# 			hashed_pswd = make_hashes(password)
-
-# 			result = login_user(username,check_hashes(password,hashed_pswd))
-# 			if result:
-
-# 				st.success("Logged In as {}".format(username))
-
-# 				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
-# 				if task == "Add Post":
-# 					st.subheader("Add Your Post")
-# 					data = view_all_users()
-# 					data = pd.DataFrame(data,columns=["Username","Password"])
-# 					st.text(data)
-                    
-
-# 				elif task == "Analytics":
-# 					st.subheader("Analytics")
-# 				elif task == "Profiles":
-# 					st.subheader("User Profiles")
-# 					user_result = view_all_users()
-# 					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-# 					st.dataframe(clean_db)
-# 			else:
-# 				st.warning("Incorrect Username/Password")
-
-
-
-
-
-# 	elif choice == "SignUp":
-# 		st.subheader("Create New Account")
-# 		new_user = st.text_input("Username")
-# 		new_password = st.text_input("Password",type='password')
-
-# 		if st.button("Signup"):
-# 			create_usertable()
-# 			add_userdata(new_user,make_hashes(new_password))
-# 			st.success("You have successfully created a valid Account")
-# 			st.info("Go to Login Menu to login")
-
-
+        signup()
 
 if __name__ == '__main__':
     main()
